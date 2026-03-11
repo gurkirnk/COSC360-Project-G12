452eb7d76bb4ea725d7446973529b9a2555c6a71
alterednode <75056546+alterednode@users.noreply.github.com>
2026-03-10 19:06:00 -0700

    Move Searchbar component and display location

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import Breadcrumbs from "./Breadcrumbs/Breadcrumbs";
 import HeaderNav from "./HeaderNav/HeaderNav";
-import SearchBar from "./SearchBar/SearchBar";
 import "./Header.css";
 
 export default function Header() {
@@ -8,7 +7,6 @@ export default function Header() {
     <header>
         <Breadcrumbs />
         <HeaderNav />
-        <SearchBar />
         <hr />
     </header>
   );
--- frontend/src/pages/BrowsePage.jsx
+++ frontend/src/pages/BrowsePage.jsx
@@ -1,8 +1,10 @@
+import SearchBar from '../components/SearchBar';
 export default function BrowsePage() {
   return (
     <>
       <h1>Browse</h1>
       <p>This is the new browse page at /browse.</p>
+      <SearchBar />
     </>
   );
 }
