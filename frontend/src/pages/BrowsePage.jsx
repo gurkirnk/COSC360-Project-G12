ef87ca6b1d2f65792ef4adfefc52ba565df1de34
sawyercairns <mrsawyercairns@gmail.com>
2026-03-15 12:14:37 -0700

    Improvements and genre
    -Improvements to handling for search params (now appear in query url
    -genre now functional, in need of select list or other)

--- frontend/src/pages/BrowsePage.jsx
+++ frontend/src/pages/BrowsePage.jsx
@@ -1,39 +1,44 @@
 import { useEffect, useState } from 'react';
 import SearchBar from '../components/SearchBar';
 import { browseListings } from '../lib/api/features/browse';
+import { useSearchParams } from "react-router-dom";
 
 //TODO: Allow parameters (genre, search), all the way to the backend
-//TODO: Make search bar functional
 /*TODO: Format listings/proper empty responce handling (I'll take care of this, just difficult to do when there is no way to create listings in the db)*/
 export default function BrowsePage() {
 
   const [listings, setlistings] = useState([]);
-  const [search, setSearch] = useState("");
-  const [genre, setGenre] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams(window.location.search);
 
-  async function fetchListings() {
-    console.log("fetching...");
-    try {
-      const response = await browseListings({ search, genre });
-      setlistings(response.data || response); 
-    } catch (error) {
-
-    } finally {
-
-    }
-  }
+  const search = searchParams.get("search") || "";
+  const genre = searchParams.get("genre") || "";
 
   useEffect(() => {
+
+    async function fetchListings() {
+      console.log(genre);
+      console.log(search);
+      try {
+        const response = await browseListings({ search, genre });
+        setlistings(response.data || response);
+      } catch (error) {
+
+      } finally {
+
+      }
+    }
+
+    
     fetchListings();
-  }, [search,genre]);
+  }, [search, genre]);
 
 
   return (
     <>
       <h1>Browse</h1>
-      <SearchBar SearchBarClicked={(newSearch) => setSearch(newSearch)}/>
+      <SearchBar SearchBarClicked={(newSearch) => setSearchParams({search: newSearch, genre: genre})} />
       <p>
-        {listings.toString()}
+        {JSON.stringify(listings)}
       </p>
     </>
   );
