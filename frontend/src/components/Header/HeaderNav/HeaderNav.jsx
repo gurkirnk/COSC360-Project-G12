6e22b4b2f9aeb36dbd059a206de780ed8f07cc9f
alterednode <75056546+alterednode@users.noreply.github.com>
2026-03-24 14:31:52 -0700

    easy logout button in nav

--- frontend/src/components/Header/HeaderNav/HeaderNav.jsx
+++ frontend/src/components/Header/HeaderNav/HeaderNav.jsx
@@ -1,8 +1,15 @@
 import RegistrationSwitcher from '../../RegistrationSwitcher/RegistrationSwitcher';
 import './HeaderNav.css';
 import { NavLink } from 'react-router-dom';
+import { useAuth } from '../../../contexts/useAuth';
 
 export default function HeaderNav() {
+  const { logout } = useAuth();
+
+  async function handleLogout() {
+    await logout();
+  }
+
   return (
     <nav className="main-navigation" aria-label="main navigation">
       <RegistrationSwitcher
@@ -12,6 +19,7 @@ export default function HeaderNav() {
             <li className="main-navigation-item"><NavLink to="/list">Make A Listing</NavLink></li>
             <li className="main-navigation-item"><NavLink to="/browse">Browse</NavLink></li>
             <li className="main-navigation-item"><NavLink to="/account">Account</NavLink></li>
+            <li className="main-navigation-item"><button type="button" onClick={handleLogout}>Logout</button></li>
           </ul>}
         unregisteredComponent={
 
